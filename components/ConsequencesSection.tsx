6c00c3314f923447398e7e741825299d090c3632
wesleygatinho <wesley.gatinho@discente.ufma.br>
2025-06-15 16:54:03 -0300

    update images

--- components/ConsequencesSection.tsx
+++ components/ConsequencesSection.tsx
@@ -8,7 +8,7 @@ const ConsequencesSection: React.FC = () => {
     <SectionContainer title="Impactos das Altas Taxas de CO₂ no Planeta" className="bg-white" id="consequencias">
       <div className="space-y-16 md:space-y-20">
         <ImageTextLayout 
-          imageUrl="https://picsum.photos/seed/poluicao_cidade/800/450"
+          imageUrl="https://s4.static.brasilescola.uol.com.br/be/2022/10/poluicao-do-ar.jpg"
           altText="Cidade coberta por uma densa camada de poluição"
           caption="A poluição atmosférica urbana é um sintoma visível do excesso de emissões."
         >
@@ -19,7 +19,7 @@ const ConsequencesSection: React.FC = () => {
         </ImageTextLayout>
 
         <ImageTextLayout 
-          imageUrl="https://picsum.photos/seed/desmatamento_amazonia/800/450"
+          imageUrl="https://jornal.usp.br/wp-content/uploads/2022/09/19092022_desmatamento-amazonia2.jpg"
           altText="Vista aérea de área desmatada na floresta tropical"
           caption="Desmatamento e queimadas aceleram a crise climática."
           reversed 
@@ -31,7 +31,7 @@ const ConsequencesSection: React.FC = () => {
         </ImageTextLayout>
 
         <ImageTextLayout 
-          imageUrl="https://picsum.photos/seed/seca_brasil/800/450"
+          imageUrl="https://s2-g1.glbimg.com/EXLvrsKgYfKAweAuB2VQcn2FDOI=/0x0:3000x2000/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2024/c/z/dimJ8YQ52SQgzVwFiZWQ/seca-amazonas-2024-08-22t113244z.jpg"
           altText="Leito de rio completamente seco com solo rachado"
           caption="A seca extrema no Brasil em 2024 é um alerta dos impactos já presentes."
         >
